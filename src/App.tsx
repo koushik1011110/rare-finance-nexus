3a1a98205c93e21a1b9d0b7907ad147b32c5d71c
Koushik <121817251+koushik1011110@users.noreply.github.com>
2025-08-26 18:09:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -85,8 +85,8 @@ import LeadEnquiry from "./pages/lead/LeadEnquiry";
 import AddLead from "./pages/lead/AddLead";
 
 // Invoice pages
-import CreateInvoice from "./pages/invoices/CreateInvoice";
-import AllInvoices from "./pages/invoices/AllInvoices";
+import CreateInvoice from "./pages/invoices/invoice";
+import AllInvoices from "./pages/invoices/index";
 
 const queryClient = new QueryClient();
 
